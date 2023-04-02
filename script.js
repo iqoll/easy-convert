ee70c0103db0f48cd6e8affe85f06ac3c73a5b96
iqolll <muhammadhaiqal1802@gmai.com>
2023-04-02 08:49:18 +0700

    add convert fucntion from ft

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const ftRef = document.getElementById('ft');
 const cmRef = document.getElementById('cm');
 const inRef = document.getElementById('in');
+
+// Functions to convert
+let convertFromFt = () => {
+    let ft = ftRef.value;
+
+    // toFixed(2) limits the digits to 2.
+    cmRef.value = (ft * 30.48).toFixed(2);
+    inRef.value = (ft * 12).toFixed(2);
+}
+
+
+ftRef.addEventListener('input', convertFromFt);
