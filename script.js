d564c583d84ce3bbcfd6b91e58e0993261031a85
iqolll <muhammadhaiqal1802@gmai.com>
2023-04-04 08:08:42 +0700

    add Weigh converter function

--- script.js
+++ script.js
@@ -64,3 +64,29 @@ let convertFromIn = () => {
 ftRef.addEventListener('input', convertFromFt);
 cmRef.addEventListener('input', convertFromCm);
 inRef.addEventListener('input', convertFromIn);
+
+// Function to convert weight
+let convertFromKg = () => {
+    kg = kgRef.value;
+
+    lbRef.value = (kg * 2.205).toFixed(2);
+    ozRef.value = (kg * 35.274).toFixed(2);
+}
+
+let convertFromLb = () => {
+    lb = pdRef.value;
+
+    kgRef.value = (lb / 2.205).toFixed(2);
+    ozRef.value = (lb * 16).toFixed(2);
+}
+
+let convertFromOz = () => {
+    oz = ozRef.value;
+
+    kgRef.value = (oz / 35.274).toFixed(2);
+    lbRef.value = (oz / 16).toFixed(2);
+}
+
+kgRef.addEventListener('input', convertFromKg);
+lbRef.addEventListener('input', convertFromLb);
+ozRef.addEventListener('input', convertFromOz);
